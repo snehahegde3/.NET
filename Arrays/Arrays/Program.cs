67b6539deccfb7f4b9d9a366c375808c4fd13698
Sneha Hegde <snehahegde33@gmail.com>
2022-12-28 00:19:43 +0530

    multidimensional arrays

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 namespace Arrays
 {
@@ -15,7 +16,8 @@ namespace Arrays
             ForEach();
             Console.WriteLine();
             ForEach_Switch_Challenge();
-
+            Console.WriteLine();
+            MultiDimensionalArrays();
 
             Console.ReadKey();
         }
@@ -96,6 +98,81 @@ namespace Arrays
                 Console.WriteLine("Invalid input and data type");
             }
             
+
+        }
+
+        public static void MultiDimensionalArrays()
+        {
+            string[,] matrix; 
+            int[, , ] threeD;
+            int[, ,] arr3D = new int[, ,] 
+            {
+                { 
+                    { 0, 1, 6 },
+                    { 2, 3, 7 } 
+                },
+                {   { 4, 5, 8 },
+                    { 9, 10, 11 } 
+                }
+            };
+
+            Console.WriteLine("First element: " + arr3D[0, 0, 0] + " Rank: " + arr3D.Rank);
+
+
+            ////foreach does not work for multidimensional arrays 
+            //foreach(dynamic i in arr2D)
+            //{
+            //    foreach(dynamic j in i)
+            //    {
+            //        Console.WriteLine(j);
+            //    }
+            //}
+
+            //var is the same as auto in c++
+
+            //how does THIS WORK ???
+
+            
+            foreach(var i in arr3D) 
+            {
+                Console.WriteLine(i);
+            }
+            
+            //wHAT? HOW?
+            foreach (dynamic i in arr3D)
+            {
+                Console.WriteLine(i);
+            }
+
+            //ooo apparently it goes from the top left to bottom right 
+            //what kinda vood00 shit 
+
+            /*
+             * VAR
+             * The type of the variable is decided by the compiler at compile time
+             * The variable of this type should be initialized at the time of declaration. So that the compiler will decide the type of the variable according to the value it initialized.
+             * 
+             * DYNAMIC
+             * The type of the variable is decided by the compiler at run time.
+             * The variable of this type need not be initialized at the time of declaration. Because the compiler does not know the type of the variable at compile time
+             * 
+             */
+
+            //GetLength(dimension)
+            for(int i = 0; i < arr3D.GetLength(0); i++)
+            {
+                //outer loop
+                for(int j = 0; j < arr3D.GetLength(1); j++)
+                {
+                    //rows 
+                    for(int k = 0; k < arr3D.GetLength(2); k++)
+                    {
+                        //columns 
+                        arr3D[i, j, k] += 10;
+                        Console.WriteLine($"{i}, {j}, {k}, {arr3D[i, j, k]}");
+                    }
+                }
+            }
 
         }
     }
