142b673916c0ef84cb2809fe1d89a2b0afcf887a
Sneha Hegde <snehahegde33@gmail.com>
2023-01-19 23:27:24 +0530

    Inheritance initial commit

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        //defines a class interms of another class 
+
+        //code reuse: more efficient, less coding time
+
+        /*      Base Class
+                    |
+                    V
+              Derived Class
+        */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
