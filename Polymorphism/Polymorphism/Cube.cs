2170fdea099c42cf38602e30ca82b2f6e5228ac6
Sneha Hegde <60070453+snehahegde3@users.noreply.github.com>
2023-02-02 19:06:23 +0530

    Abstraction

---
+++ Polymorphism/Polymorphism/Cube.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Cube: Shape
+    {
+        public double Length { get; set; }
+
+        public Cube(double length):base("Cube")
+        {
+            Length = length;
+        }
+        // now this MUST implement Volume() 
+        public override double Volume()
+        {
+            //return Length * Length * Length;
+            return Math.Pow(Length, 3);
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"\nThis is a {Name} with Length {Length}");
+        }
+    }
+}
--- Polymorphism/Polymorphism/Shape.cs
+++ Polymorphism/Polymorphism/Shape.cs
@@ -6,11 +6,18 @@ namespace Polymorphism
 	{
 		// we cant create or instantiate objects from an abstract class
 		public string Name { get; set; }
+
+		public Shape(string name)
+		{
+			Name = name;
+		}
 		public virtual void GetInfo()
 		{
 			Console.WriteLine($"\nThis is a {Name}");
 		}
 
+		//abstract method 
+		public abstract double Volume(); 
 		
 	}
 }
