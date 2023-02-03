a3fb8eb2569901b44f145c18e9234c496822421d
Sneha Hegde <snehahegde33@gmail.com>
2023-02-03 12:03:58 +0530

    Reading from text files

---
+++ TextFiles/TextFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TextFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Text files: Reading: ");
+
+            string text = System.IO.File.ReadAllText(@"D:\C#\TextFiles\Assets\TextFile.txt");
+
+            Console.WriteLine($"Text file contains the following text:\n{text}");
+
+            string[] lines = System.IO.File.ReadAllLines(@"D:\C#\TextFiles\Assets\TextFile.txt");
+            int num = 0;
+            foreach(string line in lines)
+            {
+                Console.WriteLine($"Line {num++} : {line}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
