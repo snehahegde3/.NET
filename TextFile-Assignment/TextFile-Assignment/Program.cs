4d50f4add735348d65cf802f796e9306c687d399
Sneha Hegde <snehahegde33@gmail.com>
2023-02-04 12:04:33 +0530

    stream writers

---
+++ TextFile-Assignment/TextFile-Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Tracing;
+using System.IO;
+
+namespace TextFile_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input_lines = File.ReadAllLines(@"D:\C#\TextFile-Assignment\Assets\input.txt");
+            
+            foreach(string line in input_lines)
+            {
+                if (line.Contains("split"))
+                {
+                    string[] words = line.Split(' ');
+                    using (StreamWriter output = new StreamWriter(@"D:\C#\TextFile-Assignment\Assets\output.txt", append: true))
+                    {
+                        if (words.Length > 4)
+                        {
+                                Console.Write(words[4] + " ");
+                                output.WriteLine(words[4]);
+
+                        }
+
+                    }
+                }
+                
+            }
+
+            Console.ReadKey();
+                
+        }
+    }
+}
