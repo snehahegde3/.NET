d73f59d8fd1451957040d20e22e0da6fd56ed2e5
Sneha Hegde <60070453+snehahegde3@users.noreply.github.com>
2023-02-07 11:03:08 +0530

    threads init

---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Threads");
+            Thread.Sleep(10000);
+            new Thread(() =>
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(11000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(12000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+            
+
+            var taskCompletionSource = new TaskCompletionSource<int>();
+
+        }
+    }
+}
